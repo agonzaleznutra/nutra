3e0d270a0d0dc24c898a67f119f5a92b13a989a0
Alvaro Gonzalez <alvaro.gonzalezv@ansv.gov.co>
2022-11-23 16:00:39 -0500

    sett

--- nutra/nutra/settings.py
+++ nutra/nutra/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y*@it+rsxm5_wwq*b#1qa9f@z(x^1ypzn%xynx4)_myjf$f0s$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
