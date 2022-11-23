a0999accf8568ef588f4e7f528997bf9c88874c8
Alvaro Gonzalez <alvaro.gonzalezv@ansv.gov.co>
2022-11-23 17:26:42 -0500

    get_rec

--- nutra/recomendador/views.py
+++ nutra/recomendador/views.py
@@ -4,6 +4,7 @@ import json
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+@csrf_exempt
 def recomendacion(request):
     print("controladooo alvaro",request.POST)
     retorno = {"s1":request.POST["cat1"],"s2":request.POST["cat2"],"s3":request.POST["cat3"],"s4":request.POST["cat4"]}
