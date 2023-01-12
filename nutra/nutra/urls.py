d883dd918f3f2ef4aeebfd54d5849ec148995c03
Alvaro Gonzalez <alvaro.gonzalezv@ansv.gov.co>
2023-01-12 17:12:59 -0500

    asd

--- nutra/nutra/urls.py
+++ nutra/nutra/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import url
-from . import views
 
 urlpatterns = [
     path('app/', include('recomendador.urls')),
     path('admin/', admin.site.urls),
-    path('get_recom', views.recomendacion, name='get_recom'),
-    url(r'^health', views.health),
+    #path('get_recom', views.recomendacion, name='get_recom'),
+    #url(r'^health', views.health),
 ]
