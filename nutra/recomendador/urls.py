1931a64c458a8160eb86fcd8b1b74829107a2fd6
Alvaro Gonzalez <alvaro.gonzalezv@ansv.gov.co>
2023-01-12 17:11:20 -0500

    cinn

--- nutra/recomendador/urls.py
+++ nutra/recomendador/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('get_recom', views.recomendacion, name='get_recom'),
-    url(r'^health', views.health),
+    path('', views.index, name='index')
 
 ]
