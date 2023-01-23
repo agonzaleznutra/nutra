d60b253c7420d0fb548dfbcc0733db0b1e6da3a4
Alvaro Gonzalez <alvaro.gonzalezv@ansv.gov.co>
2023-01-23 16:42:39 -0500

    asd

--- nutra/recomendador/views.py
+++ nutra/recomendador/views.py
@@ -4,7 +4,33 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
 from pymongo import MongoClient
 import urllib.parse
-
+import datetime
+def extraccion_atributos_en_objeto(obj):
+    
+    res = {}
+    for o in obj:
+        if ".json" in o:
+            print(o)
+            res[o.split(".json")[0].strip()]  = json.loads(obj[o])
+        elif "[]" in o:
+            lista = obj.getlist(o, [])
+            if len(lista) == 1 and lista[0].strip() == '':
+                lista = []
+            nwlista = []
+            for j in lista:
+                try:
+                    nwlista.append(json.loads(j))
+                except:
+                    nwlista.append(j.strip())
+            res[o.split("[]")[0].strip()] = nwlista
+        else:
+            if o == "id" and obj[o]== "null":
+                res[o] = ""
+            else:
+                res[o.strip()] = obj[o].strip()
+    
+    res["fecha"] = datetime.datetime.now().strftime("%Y-%m-%d")
+    return res
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 @csrf_exempt
@@ -17,13 +43,13 @@ def recomendacion(request):
 	)
 @csrf_exempt
 def load_content(request):
-    print(request.POST)
+    salida = extraccion_atributos_en_objeto(request.POST)
     
     username = urllib.parse.quote_plus('aleja_user')
     password = urllib.parse.quote_plus('02-10-91aldigovE')
 
     mongo_client = MongoClient(str("mongodb://%s:%s@127.0.0.1") % (username, password))
-    mongo_client.nutra.contenidos.insert_one(request.POST)
+    mongo_client.nutra.contenidos.insert_one(salida)
     return HttpResponse (
 		json.dumps({"retorno":"ok"}),
 		content_type = "application/json"
