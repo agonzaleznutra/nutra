797ceaf175089c79f2d99ea42eb9a6cd5c884459
Alvaro Gonzalez <alvaro.gonzalezv@ansv.gov.co>
2023-01-23 16:23:59 -0500

    asd

--- nutra/recomendador/views.py
+++ nutra/recomendador/views.py
@@ -8,18 +8,15 @@ def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 @csrf_exempt
 def recomendacion(request):
-    print("controladooo alvaro",request.POST,request.GET)
-    retorno = {"s1":request.POST.get('cat1', 'No data found'),
-    "s2":request.GET.get('cat2', 'No data found'),
-    "s3":request.GET.get('cat3', 'No data found'),
-    "s4":request.GET.get('cat4', 'No data found')}
+    print(request.POST)
+    
     return HttpResponse (
-		json.dumps(retorno),
+		json.dumps({"retorno":"ok"}),
 		content_type = "application/json"
-	)	
+	)
 @csrf_exempt
 def load_content(request):
-    print("controladooo alvaro222",request.POST)
+    print(request.POST)
     
     return HttpResponse (
 		json.dumps({"retorno":"ok"}),
