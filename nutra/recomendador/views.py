2676bfbdd764e3eb0ee0faf76cd21a0500779cc1
Alvaro Gonzalez <alvaro.gonzalezv@ansv.gov.co>
2023-01-23 16:33:14 -0500

    asd

--- nutra/recomendador/views.py
+++ nutra/recomendador/views.py
@@ -2,7 +2,8 @@ from django.http import HttpResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
-
+from pymongo import MongoClient
+import urllib.parse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -18,6 +19,11 @@ def recomendacion(request):
 def load_content(request):
     print(request.POST)
     
+    username = urllib.parse.quote_plus('aleja_user')
+    password = urllib.parse.quote_plus('02-10-91aldigovE')
+
+    mongo_client = MongoClient(str("127.0.0.1") % (username, password))
+    mongo_client.contenidos.insert_one(request.POST)
     return HttpResponse (
 		json.dumps({"retorno":"ok"}),
 		content_type = "application/json"
