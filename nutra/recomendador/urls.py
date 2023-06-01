4e1dfbd448e2215ee13daca2f78567e864f49772
agonzaleznutra <agonzales@nutrapp.com.co>
2023-05-31 21:27:55 -0500

    asd

--- nutra/recomendador/urls.py
+++ nutra/recomendador/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^/filter', views.busqueda_contenido, name='filter'),
     url(r'^/get_recom', views.recomendacion_home, name='get_recom'),
-    url(r'^/get_recom_video', views.recomendacion_video, name='get_recom_video'),
+    url(r'^/recom_video', views.recomendacion_video, name='get_recom_video'),
     url(r'^/load_content', views.view_crear_contenido, name='load_content'),
     url(r'^/set_consumo', views.view_crear_consumo, name='set_consumo'),
 
