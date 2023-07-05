fcd08c4389059128368c57e31b0365b1191c620a
agonzaleznutra <agonzales@nutrapp.com.co>
2023-07-05 15:06:21 -0500

    asd

--- nutra/recomendador/controller.py
+++ nutra/recomendador/controller.py
@@ -79,7 +79,8 @@ class views_control:
         return crud().create_relacion(objeto )
     def crear_usuario(self,salida):
         print("salida.....",salida)
-        results = list(crud().read_usuario_by_id(salida["idUser"]))
+        salida["id"] = salida["idUser"]
+        results = list(crud().read_usuario_by_id(salida["id"]))
         ret = ""
         if len(results)==0:
             ret = crud().create_usuario(salida)
